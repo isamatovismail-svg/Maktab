6a438610efd111a1e139b7800b487e66ca2a36df
isamatovismail-svg <isamatovismail@gmail.com>
2026-07-29 13:00:24 +0500

    feat: yangi dizayn qo'shildi va xatoliklar tuzatildi
    - school/ papkasidan yangi CSS dizayn qo'llandi
    - Barcha templatelar yangi uslubga o'tkazildi
    - Login xato xabari tuzatildi (form.errors -> error)
    - student_delete va lesson_delete da object -> student/lesson tuzatildi
    - Sidebar ga 5 ta menyu elementi qo'shildi (faqat Talabalar va Darslar ishlaydi)
    - forms.py dan Bootstrap class olib tashlandi

--- apps/forms.py
+++ apps/forms.py
@@ -1,19 +1,23 @@
 from django import forms
 from .models import Student, Lesson
 
-class BootstrapModelForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs.update({'class': 'form-control'})
 
-class StudentForm(BootstrapModelForm):
+class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ['first_name', 'last_name', 'phone']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'Dilnoza'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Yusupova'}),
+            'phone': forms.TextInput(attrs={'placeholder': '+998 90 123 45 67'}),
+        }
 
-class LessonForm(BootstrapModelForm):
+
+class LessonForm(forms.ModelForm):
     class Meta:
         model = Lesson
         fields = ['title', 'teacher_name']
-
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Python asoslari'}),
+            'teacher_name': forms.TextInput(attrs={'placeholder': 'Aziz Karimov'}),
+        }
